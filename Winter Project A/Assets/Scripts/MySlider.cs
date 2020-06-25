6b0390f4d7432f5099c317db6053ec9852f8a104
Jonas_QD <richmondjonas@gmail.com>
2020-06-25 15:57:30 -0700

    AddListener scene completed

---
+++ Winter Project A/Assets/Scripts/MySlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mySlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Text textSldr;
+    Slider mySld;
+    
+    void Start()
+    {
+        mySld = GetComponent<Slider>();
+        mySld.onValueChanged.AddListener(delegate
+        {
+            
+            textSldr.text = "Value: " + mySld.value;
+
+        });
+    }
+
+    // Update is called once per frame
+    void OnDestroy()
+    {
+        mySld.onValueChanged.RemoveAllListeners();
+    }
+}
